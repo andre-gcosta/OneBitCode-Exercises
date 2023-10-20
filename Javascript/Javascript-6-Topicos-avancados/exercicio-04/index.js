832df7c8d1434a2404b994ebd9197832e4206a75
andre-gcosta <andre.gcosta1@gmail.com>
2023-10-20 17:31:14 -0300

    css renewed

--- Javascript/Javascript-6-Topicos-avancados/exercicio-04/index.js
+++ Javascript/Javascript-6-Topicos-avancados/exercicio-04/index.js
@@ -1,3 +1,22 @@
+let transactionsArray = []
+
+function createEditTransactionBtn(transactionData) {
+    const edit = document.createElement('button')
+    edit.classList.add('.transaction-edit')
+    edit.textContent = 'Editar'
+    edit.addEventListener('click', () => {
+        document.querySelector('#id').value = transactionData.id
+        document.querySelector('#name').value = transactionData.name
+        document.querySelector('#amount').value = transactionData.amount
+    })
+    return edit
+}
+
+function calculateBalance() {
+    const balanceSpan = document.querySelector('#balance')
+    console.log(transactionsArray)
+}
+
 function renderTransaction(transactionData) {
     const transaction = document.createElement('article')
     transaction.classList.add('transaction')
@@ -8,18 +27,27 @@ function renderTransaction(transactionData) {
     const amount = document.createElement('h3')
     amount.classList.add('transaction-amount')
     amount.textContent = transactionData.amount
+    const edit = createEditTransactionBtn(transactionData)
 
-    transaction.append(name, amount)
+    transaction.append(name, amount, edit)
     document.querySelector('#transactions').append(transaction)
 }
 
 async function fetchTransactions() {
     const transactions = await fetch("http://localhost:3000/transactions").then(result => result.json())
     transactions.forEach(renderTransaction)
+    transactionsArray.push(transactions)
+}
+
+async function setup() {
+    const results = await fetch("http://localhost:3000/transactions")
+    transactionsArray.push(results)
+    transactionsArray.forEach(renderTransaction)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchTransactions()
+
 })
 
 const form = document.querySelector('form')
@@ -28,20 +56,38 @@ form.addEventListener('submit', async (ev) => {
     ev.preventDefault()
 
     const transactionData = {
+        id: document.querySelector('#id').value,
         name: document.querySelector('#name').value,
-        amount: document.querySelector('#amount').value
+        amount: parseFloat(document.querySelector('#amount').value)
     }
 
-    const response = await fetch('http://localhost:3000/transactions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(transactionData)
-    })
+    if (transactionData.id) { // editar a transação com esse id
+        const response = await fetch(`http://localhost:3000/transactions/${transactionData.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(transactionData)
+        })
+        const savedTransaction = await response.json()
+        renderTransaction(savedTransaction)
+        transactionsArray[0][id] = savedTransaction
+        // transactionsArray[0].push(savedTransaction)
+        calculateBalance()
+    } else { // criar nova transação
+        const response = await fetch('http://localhost:3000/transactions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(transactionData)
+        })
+        const savedTransaction = await response.json()
+        renderTransaction(savedTransaction)
+        transactionsArray[0].push(savedTransaction)
+        calculateBalance()
+    }
 
-    const savedTransaction = await response.json()
-    renderTransaction(savedTransaction)
     form.reset()
-    console.log(savedTransaction)
 })
+
