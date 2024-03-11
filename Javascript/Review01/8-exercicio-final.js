736ccc04efd63a877247c27f2043fe9bcec1eedc
andre-gcosta <andre.gcosta1@gmail.com>
2024-03-11 09:44:31 -0300

    removido variaveis desnecessárias, adicionado reduce em viewVacancy e viewAllVacancies

--- Javascript/Review01/8-exercicio-final.js
+++ Javascript/Review01/8-exercicio-final.js
@@ -1,12 +1,19 @@
 vacancies = []
 
 function viewAllVacancies() {
-    const allVacancies = vacancies.map(function(element, index) {
-        const name = element.name
-        const registered = element.namesRegistered.length??0
-        return 'Index: ' + index + '\nNome: ' + name + '\nCandidatos registrados: ' + registered + '\n\n'
-    })
-    window.alert('Lista de vagas: ' + '\n\n' + allVacancies)
+    // const allVacancies = vacancies.map(function(element, index) {
+    //     const name = element.name
+    //     const registered = element.namesRegistered.length??0
+    //     return 'Index: ' + index + '\nNome: ' + name + '\nCandidatos registrados: ' + registered + '\n\n'
+    // })
+    // window.alert('Lista de vagas: ' + '\n\n' + allVacancies)
+    const allVacancies = vacancies.reduce(function(accum, element, index) {
+        accum += 'Index: ' + index
+        accum += '\nNome: ' + element.name
+        accum += '\nCandidatos registrados: ' + (element.namesRegistered.length??0) + '\n\n'
+        return accum
+    }, 'Lista de vagas: \n\n')
+    window.alert(allVacancies)
 }
 
 function createNewVacancy (name, description, limitDate) {
