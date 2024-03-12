3dfc02924117d7a62c0263c03ab851505a8caf02
andre-gcosta <andre.gcosta1@gmail.com>
2024-03-12 20:14:31 -0300

    correções revisao02

--- Javascript/Review02/7-exercicios-estatistica.js
+++ Javascript/Review02/7-exercicios-estatistica.js
@@ -4,8 +4,8 @@ const media = (...params) => {
 }
 
 const mediaPonderada = (...params) => {
-    const soma = params.reduce((accum, param) => accum + (param.value * param.weight), 0)
-    const denominador = params.reduce((accum, param) => accum + param.weight, 0)
+    const soma = params.reduce((accum, { value, weight }) => accum + (value * weight), 0)
+    const denominador = params.reduce((accum, param) => accum + (param.weight??1), 0)
     return soma/denominador
 }
 
@@ -26,20 +26,20 @@ const mediana = (...params) => {
     }
 }
 
-const moda = (...params) => {
+const moda2 = (...params) => {
     let findBiggestNumber = []
     const orderedParams = params.sort((a, b)=> a-b)
-    for (let i=0; i<orderedParams.length; i++) {
+    orderedParams.map((element, i, array) => {
         if (i == 0) {
             findBiggestNumber.push(1);
         }
-        else if (orderedParams[i] == orderedParams[i-1]) {
+        else if (array[i] == array[i-1]) {
             findBiggestNumber.push(findBiggestNumber[i-1] + 1)
         }
         else {
             findBiggestNumber.push(1)
         }
-    }
+    })
     let numberOfRepetitions = 0
     let numberIndex = 0
     for (let i=0; i<findBiggestNumber.length; i++) {
@@ -51,9 +51,15 @@ const moda = (...params) => {
     return orderedParams[numberIndex]
 }
 
+const moda = (...numbers) => {
+    const numberAndQuantities = numbers.map(number => [number, numbers.filter(numberIsRepeated => (numberIsRepeated === number)).length])
+    numberAndQuantities.sort((a, b) => b[1]-a[1])
+    return numberAndQuantities[0][0]
+}
+
 // const calcular = media(8,7,5,3,2,10,35)
 // const calcular = mediana(1,2)
-// const calcular = moda(1, 3, 4, 5,10,245,82,71,42,7,8,12013,555)
+const calcular = moda(1, 2, 3, 4, 5, 5, 5, 6, 7,7, 10, 8, 9,9,9,9)
 // const calcular = mediaPonderada({value: 5, weight: 1}, {value: 5, weight: 1}, {value: 6, weight: 1}, {value: 5, weight: 1}, {value: 5, weight: 1}, {value: 3, weight: 5})
 
-// console.log(calcular)
+console.log(calcular)
